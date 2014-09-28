40aa918f494bfe342e05b55aa2282b3d29cbbb82
JohnOnSoftware <zhong.wu@autodesk.com>
2014-09-28 20:58:56 +0800

    Update with 2nd version
    2nd version of the game

---
+++ Readme.md
@@ -0,0 +1,20 @@
+Small game project with Autodesk logos, it is just finding out a correct
+logos in different positions. And will calculate your ranking of click number 
+when you complete the game.  
+
+To play with this, you need to do the following steps:
+
+1. be sure to install node.js first
+
+2. use "npm install" to install all the dependent modules
+
+3. Install Mongodb, and run it as mongod from command line (make sure you create the data folder as c:\data\db\ by yourself)
+
+4. If you want to debug NodeJs at server, please refer my blog http://www.cnblogs.com/johnonsoftware/articles/3995174.html
+
+5. run server by command "node restApi.js"
+
+6. Run http://localhost:4711/, you can play the game, and it will give you the rank when you successfully passed. (to save time, you just need to pick one pair)
+
+
+Note: This is just a technique verification, still much to improve. 
---
+++ restApi.js
@@ -0,0 +1,199 @@
+// Module dependencies.
+var application_root = __dirname,
+    express = require( 'express' ), //Web framework
+    path = require( 'path' ),           //Utilities for dealing with file paths
+	fs   = require( 'fs' ),
+	// mongoskin = require('mongoskin'),
+	bodyParser = require('body-parser'),
+    mongoose = require( 'mongoose' );   
+	
+
+//Create server
+var app = express();
+
+// Configure server
+// app.configure( function() {
+    // //parses request body and populates request.body
+    // app.use( express.bodyParser() );
+
+    // //checks request.body for HTTP method overrides
+    // app.use( express.methodOverride() );
+
+    // //perform route lookup based on url and HTTP method
+    // app.use( app.router );
+
+    // //Show all errors in development
+    // app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
+// });
+
+
+//Connect to database
+var db = mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+//Schema
+var UsersSchema = new mongoose.Schema({
+    name: String,
+    score: Number
+});
+
+//Model
+mongoose.model( 'Users', UsersSchema );
+
+var User = mongoose.model('Users');
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use( bodyParser.urlencoded() ); // to support URL-encoded bodies
+
+app.use(express.static(__dirname + '/public'));
+
+// global varible
+var cache;
+
+//Router
+app.all('*', function(req, res, next){
+  if (!req.get('Origin')) return next();
+  // use "*" here to accept any origin
+  res.set('Access-Control-Allow-Origin', 'http://localhost');
+  // res.set('Access-Control-Allow-Methods', 'GET');
+  // res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  // res.set('Access-Control-Allow-Max-Age', 3600);
+  // if ('OPTIONS' == req.method) return res.send(200);
+  next();
+});
+
+
+// return the view page, a problem to load script.
+app.get( '/', function(req, res){
+	if(cache == null)
+	{
+		var indexFile = './public/index.html';
+		fs.exists( indexFile, function(exists){
+			if(exists){
+				fs.readFile(indexFile, function(err, data){
+					if(err){
+						res.statusCode = 404;
+						res.end('page not found!');
+					}
+					else{
+						cache = data;
+						res.setHeader( 'content-type', 'text/html' );
+						res.statusCode = 200;
+						res.end(cache);							
+					}
+				})
+			}
+			else{
+				res.statusCode = 404;
+				res.end('page not found!');
+			}
+		})
+	}
+	else{
+		res.setHeader( 'content-type', 'text/html' );
+		res.statusCode = 200;
+		res.end(cache);			
+	}
+})
+
+
+//Get a single user info by user name - TBD
+app.get( '/api/users/:id', function( request, response ) {
+    var user = {
+        name: "John Wu",
+        score: "80",
+    };
+    
+    response.send(user);
+});
+
+
+//Get the rank of user
+app.get( '/api/users', function( request, response ) {
+
+	var user = new User();
+	var curName =  user.name  = request.query['name'];
+	var curScore = user.score = request.query['score'];
+
+	user.save( function(err){
+		if(err){
+			console.log(err);
+			response.status(500).send('Insert record to database failed!');
+		}
+		else{
+			User.find({}, function(err, users){
+				if(err){
+					console.log(err);
+					response.status(500).send('Get record from database failed!');
+				}
+				else{
+					var ranking = 0;
+					var userNo = users.length;
+					for( var i = 0; i < userNo; i++ ){
+						if( users[i].score > curScore )
+							++ranking;
+					}
+					var curRanking = (100* ranking )/ userNo;
+					if(curRanking.undefined )
+						response.status(500).send( "Server is not correct!" );
+					else
+						response.status(200).send( 'you defeated ' + curRanking + ' percentage people!' );
+				}
+			})
+		}
+	})
+});
+
+// Post a record, and return the rank info.
+app.post( '/api/users', function( request, response ) {
+	var user = new User();
+	// failed to get the name and score？
+	var curName  = user.name  = request.body.name;
+	var curScore = user.score = request.body.score;
+
+	user.save( function(err){
+		if(err){ 
+			console.log(err);
+			response.status(500).send('Server is wrong.');
+		}
+		else{ // get the ranking of the user, and send it back.
+			User.find({}, function(err, users){
+				if(err){
+					console.log(err);
+					response.status(500).send('Server is wrong.');
+				}
+				else{
+					var ranking = 0;
+					var userNo = users.length;
+					for( var i = 0; i < userNo; i++ ){
+						if( users[i].score > curScore )
+							++ranking;
+					}
+					var curRanking = (100* ranking )/ userNo;
+					if(curRanking.undefined )
+						response.status(500).send( "Server is not correct!" );
+					else
+						response.status(200).send( curName+' defeated ' + curRanking + ' percentage people!' );
+				}
+			});
+			console.log('created..');
+		}
+	});
+});
+
+
+//Update a user - TBD
+app.put( '/api/users/:id', function( request, response ) {
+    response.send("Updated!");
+});
+
+//Delete a user - TBD
+app.delete( '/api/users/:id', function( request, response ) {
+    response.send("Deleted");
+});
+
+//Start server
+var port = 4711;
+app.listen( port, function() {
+    console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
+});
